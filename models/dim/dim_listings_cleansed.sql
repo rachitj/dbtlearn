685f0d415c9da6fbcf3a79763517d423daf6f402
rachitj <rjauhari.contact@gmail.com>
2025-08-16 20:02:38 +0530

    Created cleansed dimensions  for hosts and listings

---
+++ models/dim/dim_listings_cleansed.sql
@@ -0,0 +1,20 @@
+With src_listings AS (
+    SELECT
+        *
+    FROM
+        {{ ref('src_listings') }}
+)
+SELECT
+    listing_id,
+    listing_name,
+    room_type,
+    CASE
+        WHEN minimum_nights= 0 then 1 
+        Else minimum_nights
+    END as minimum_nights,
+    host_id,
+    REPLACE(price_str, '$', '')::NUMBER(10, 2) as price,
+    created_at,
+    updated_at
+FROM
+    src_listings
